4d801f007ece6f90160d47e092afb14a9f9ce240
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-12 15:41:52 +0300

    Add Top 1 to return the most popular.

--- SQL/Queries/popular-genre.sql
+++ SQL/Queries/popular-genre.sql
@@ -1,8 +1,8 @@
 -- Identify the most popular genre for a given month --
 
-DECLARE @Month INT = 4;
+DECLARE @Month INT = 3;
 
-SELECT B.Genre, COUNT(*) AS [Number of books],
+SELECT TOP 1 B.Genre, COUNT(*) AS [Number of books],
 	RANK() OVER (ORDER BY COUNT(*) DESC) AS [Genre rank]
 FROM Loans L
 JOIN Books B ON L.BookID = B.BookID
