1e9b719f5b27945aef7ae767ccaa7fc62445b397
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-13 02:02:43 +0300

    Design a trigger to log an entry into a separate AuditLog table whenever a book's status changes

---
+++ SQL/Triggers/audit_log_trig.sql
@@ -0,0 +1,31 @@
+DROP TRIGGER IF EXISTS trg_AuditLogUpdate;
+GO
+
+CREATE TRIGGER trg_AuditLogUpdate
+ON Books
+AFTER UPDATE
+AS
+BEGIN
+	IF UPDATE(Current_Status)
+	BEGIN
+		INSERT INTO AuditLog (BookID, StatusChange, ChangeDate)
+		SELECT 
+			BookID,
+			'To ' + Current_Status,
+			GETDATE()
+		FROM inserted;
+	END
+END
+
+
+/* usage
+
+	SELECT * FROM AuditLog;
+
+	UPDATE Books
+	SET Current_Status =  'Available'
+	WHERE BookID = 1;
+
+	SELECT * FROM AuditLog;
+
+*/
