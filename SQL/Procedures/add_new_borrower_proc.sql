acf71a255fe892e94b5b49a4bc7901c7e244f227
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-12 17:07:46 +0300

    Streamline the process of adding a new borrower

---
+++ SQL/Procedures/add_new_borrower_proc.sql
@@ -0,0 +1,26 @@
+DROP PROCEDURE IF EXISTS  sp_AddNewBorrower
+GO
+
+CREATE PROCEDURE sp_AddNewBorrower 
+	@FirstName NVARCHAR(255), 
+	@LastName NVARCHAR(255), 
+	@Email NVARCHAR(255),
+	@DateOfBirth DATE, 
+	@MembershipDate DATE, 
+	@ErrorMessage NVARCHAR(255) OUTPUT,
+	@NewBorrowerID INT OUTPUT
+AS 
+BEGIN
+	IF EXISTS (SELECT 1 FROM Borrowers WHERE Email = @Email)
+	BEGIN
+		SET @ErrorMessage = 'A borrower with this email already exists.';
+		RETURN;
+	END
+
+	INSERT INTO Borrowers (First_Name, Last_Name, Email, Date_of_Birth, Membership_Date)
+    VALUES (@FirstName, @LastName, @Email, @DateOfBirth, @MembershipDate);
+
+	SET @NewBorrowerID = SCOPE_IDENTITY();
+	SET @ErrorMessage = 'No error. Sucessfully added';
+END;
+
