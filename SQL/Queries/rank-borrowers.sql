627d63c2811283d47163bf3e87fab5a81f72aa00
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-12 15:27:19 +0300

    Change the alias

--- SQL/Queries/rank-borrowers.sql
+++ SQL/Queries/rank-borrowers.sql
@@ -1,6 +1,6 @@
 -- Rank borrowers based on borrowing frequency --
 
-SELECT BorrowerID, Count(*) AS NumberOfBooks, 
-	DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) AS BorrowerRank
+SELECT BorrowerID, Count(*) AS [Number of books], 
+	DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) AS [Borrower rank]
 FROM Loans
 GROUP BY BorrowerID;
